d32fa655a4c43d2438d896504bb2543d7e0cbf4c
sutikovi-gor <sutikovi@gmail.com>
2022-10-04 15:35:46 +0300

    Исправил

--- src/oopsptv21/OOPSPTV21.java
+++ src/oopsptv21/OOPSPTV21.java
@@ -5,6 +5,10 @@
  */
 package oopsptv21;
 
+import myclasses.Author;
+import myclasses.Book;
+
+
 /**
  *
  * @author user
@@ -15,7 +19,16 @@ public class OOPSPTV21 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Author author1 = new Author();
+        author1.setFirstname("Aleksanr");
+        author1.setLastname("Puskin");
+        Book book1 = new Book();
+        book1.setCaption("Jevgeny Onegin");
+        Author[] authors = new Author[1];
+        authors[0]=author1;
+        book1.setAuthors(authors);
+        System.out.println(book1.toString());
+       
     }
     
 }
